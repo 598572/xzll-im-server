7d5b5d5f59b8384cfcf8031d3b2f5c59296d3ada
hzz <h163361631@163.com>
2024-07-11 22:53:12 +0800

    测试push触发jenkins部署

--- xzll-im-freestyle-way-shell.sh
+++ xzll-im-freestyle-way-shell.sh
@@ -15,9 +15,19 @@ exit_on_error() {
 
 log ">>>>>>>>>>>> 开始部署 >>>>>>>>>>>>>"
 
+
+# 确保 docker-compose 目录存在 不存在则创建
+COMPOSE_DIR="/usr/local/soft_hzz/xzll-im/jar-file/jenkins_way_build_docker_compose/"
+if [ ! -d "$COMPOSE_DIR" ]; then
+  log ">>>>>>>>>>>> 目录不存在，创建目录 $COMPOSE_DIR >>>>>>>>>>>>>"
+  mkdir -p "$COMPOSE_DIR" || { log "Failed to create directory $COMPOSE_DIR"; exit 1; }
+fi
+
 # 进入 docker-compose 目录
 log ">>>>>>>>>>>> 进入到 docker-compose 目录中 >>>>>>>>>>>>>"
-cd /usr/local/soft_hzz/xzll-im/jar-file/jenkins_way_build_docker_compose/ || { log "Failed to enter directory"; exit 1; }
+cd "$COMPOSE_DIR" || { log "Failed to enter directory $COMPOSE_DIR"; exit 1; }
+
+
 
 # 如果项目目录不存在，则执行 clone，否则执行 pull
 if [ ! -d "xzll-im" ]; then
@@ -34,8 +44,13 @@ fi
 log ">>>>>>>>>>>> 进入项目根目录 >>>>>>>>>>>>>"
 cd xzll-im/ || { log "Failed to enter project directory"; exit 1; }
 
+git fetch
+
+git checkout jenkins_build_20240709
+
 # 开始打包
 log ">>>>>>>>>>>> 开始打包 >>>>>>>>>>>>>"
+
 mvn clean package -P prod || { log "Maven build failed"; exit 1; }
 
 log ">>>>>>>>>>>> 打包完成 >>>>>>>>>>>>>"
@@ -55,9 +70,15 @@ for JAR_FILE in "${!JAR_FILES[@]}"; do
   cp "$JAR_FILE" "$TARGET_DIR" || { log "Failed to copy $JAR_FILE to $TARGET_DIR"; exit 1; }
 done
 
+
+cp docker-compose.yml ../
+
 # 返回 docker-compose 目录
 log ">>>>>>>>>>>> 返回到 docker-compose 目录 >>>>>>>>>>>>>"
 cd .. || { log "Failed to return to docker-compose directory"; exit 1; }
+
+
+
 
 # 停止所有 docker-compose 运行的容器
 log ">>>>>>>>>>>> 停止所有 docker-compose 运行的容器 >>>>>>>>>>>>>"
